dde0947a970927c61490bcf6a46a2c3fd82c2f2b
Himanshu <himanshuvenu14@gmail.com>
2024-12-22 01:20:01 +0530

    updates

--- src/components/passes/UserPasses.tsx
+++ src/components/passes/UserPasses.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { CreditCard } from "lucide-react";
 import clsx from "clsx";
 import { createClient } from "@supabase/supabase-js";
-import { QRCode } from "qrcode.react";
+import QRCode from "react-qr-code";
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL || "",
